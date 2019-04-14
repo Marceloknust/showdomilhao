07e36b99a0f47151c2ad25ac74dc1136ca03c6d7
Marceloknust <49623197+Marceloknust@users.noreply.github.com>
2019-04-14 18:38:12 -0300

    Add files via upload

---
+++ execucao.py
@@ -0,0 +1,340 @@
+from time import sleep
+
+from os import system
+
+from lista import listaPergunta, listaResposta, listaPremio, listaJogadores, listaPontuacao
+
+from random import randint
+
+from copy import deepcopy
+
+def jogo(): #função que faz as perguntas do jogo
+    errou = False
+    contador = 0
+    parar = "n"
+    contadorpulos = 0
+    denovo = "t"
+    system("cls")
+    print(f"""
+        ----------------------------------------------------------
+                Olá {nome} vamos a primeira pergunta
+        ----------------------------------------------------------
+        ---A qualquer momento digite [j] para pular a pergunta----
+        ----------------------------------------------------------
+        """)
+    if contadorpulos < 3:
+        print(f"Lembando que você ainda tem { ( 3 - contadorpulos) } pulos")
+        print()
+    else:
+        print(f"você não tem mais pulos")
+        print()
+    question = randint(0,(49-contador))
+
+    print(listaPergunta[question])
+
+    resposta = input("Sua resposta é: ").strip().lower()
+
+    while resposta not in ("a","b","c","d","e","p","j"):
+        resposta = input("""Resposta invalida.   Por favor digite uma resposta válida: """).strip().lower()
+
+    if resposta == "p":
+        parar = input(f"""Você deseja mesmo parar? [S/N]
+        Se parar você sairá com R${(listaPremio[contador] * 1000)}
+        r: """).strip().lower()
+
+        while parar not in ("s","n"):
+            parar = input(f"""Você deseja mesmo parar? [S/N]
+            Se parar você sairá com {(listaPremio[contador] * 1000)}
+            r: """).strip().lower()
+
+        if parar == "s":
+            system("cls")
+            print(f"Parabéns você saiu com R${(listaPremio[contador] * 1000)}")
+            while denovo not in ("s","n"):
+                denovo = input(f"""Você deseja jogar novamente? [S/N]
+                r: """).strip().lower()
+
+            if denovo == "s":
+                system("cls")
+
+                listaJogadores.append(nome)
+
+                listaPontuacao.append(contador)
+
+                telainicial()
+
+            else:
+                print()
+        else:
+            print()
+    elif resposta == "j":
+        if contadorpulos >= 3:
+            print("Desculpe você não tem mais pulos!")
+        else:
+            contadorpulos += 1
+
+            contador += 1
+
+            del listaPergunta[question]
+
+            del listaResposta[question]
+
+            system("cls")
+
+    elif resposta != listaResposta[question]:
+            system("cls")
+            print(f"Que pena você errou e ficou com R${(listaPremio[contador] * 500)}")
+            errou = True
+            while denovo not in ("s","n"):
+                denovo = input(f"""Você deseja jogar novamente? [S/N]
+                r: """).strip().lower()
+
+            if denovo == "s":
+                system("cls")
+
+                listaJogadores.append(nome)
+
+                listaPontuacao.append(contador)
+
+                telainicial()
+
+            else:
+                print()
+
+    else:
+        contador+=1
+
+        print(f"Parabens você acertou e foi para pergunta de R${listaPremio[contador+1] * 1000}")
+
+        sleep(2)
+
+        del listaPergunta[question]
+
+        del listaResposta[question]
+
+        question = randint(0,(49-contador))
+
+        system("cls")
+    while errou == False and contador <15 and parar == "n":
+        sleep(0.5)
+        print(f"""
+            ----------------------------------------------------------
+            {nome} vamos a {contador+1}ª pergunta
+            ----------------------------------------------------------
+            ---------A qualquer momento digite [p] para parar---------
+            ----------------------------------------------------------
+            ---A qualquer momento digite [j] para pular a pergunta----
+            ----------------------------------------------------------""")
+        if contadorpulos < 3:
+            print(f"Lembando que você ainda tem { (3 - contadorpulos) } pulos")
+        else:
+            print(f"você não tem mais pulos")
+
+        print(listaPergunta[question])
+        resposta = input("Sua resposta é: ").strip().lower()
+
+        while resposta not in ("a","b","c","d","e","p","j"):
+            resposta = input("""Resposta invalida.   Por favor digite uma resposta válida: """).strip().lower()
+
+        if resposta == "p":
+            parar = input(f"""Você deseja mesmo parar? [S/N]
+            Se parar você sairá com R${(listaPremio[contador] * 1000)}
+            r: """).strip().lower()
+
+            while parar not in ("s","n"):
+                parar = input(f"""Você deseja mesmo parar? [S/N]
+                Se parar você sairá com {(listaPremio[contador] * 1000)}
+                r: """).strip().lower()
+
+            if parar == "s":
+                system("cls")
+                print(f"Parabéns você saiu com R${(listaPremio[contador] * 1000)}")
+
+            else:
+                print()
+        elif resposta == "j":
+            if contadorpulos >= 3:
+                print("Desculpe você não tem mais pulos!")
+            else:
+
+                contadorpulos += 1
+
+                contador += 1
+
+                del listaPergunta[question]
+
+                del listaResposta[question]
+
+                system("cls")
+
+        elif resposta != listaResposta[question]:
+            system("cls")
+            print(f"Que pena você errou e ficou com R${(listaPremio[contador] * 500)}")
+            errou = True
+            while denovo not in ("s","n"):
+                denovo = input(f"""Você deseja jogar novamente? [S/N]
+                r: """).strip().lower()
+
+            if denovo == "s":
+                system("cls")
+
+                listaJogadores.append(nome)
+
+                listaPontuacao.append(contador)
+
+                telainicial()
+
+            else:
+                print()
+
+            system("cls")
+
+        else:
+            contador+=1
+
+            print(f"Parabens você acertou e foi para pergunta de R${listaPremio[contador+1] * 1000}")
+
+            sleep(2)
+
+            del listaPergunta[question]
+
+            del listaResposta[question]
+
+            question = randint(0,(49-contador))
+
+            system("cls")
+    if contador == 15:
+        print(f"""
+            ------------------------------------------------------------------------
+               Olá {nome}, parabéns você chegou a pergunta de um milhão de reais!
+            ------------------------------------------------------------------------
+            -----------------Está pronto(a) para o tudo ou nada?--------------------
+            ----------------Lembrando que se você errar perde tudo------------------
+            ------------------------------------------------------------------------
+            """)
+        question = randint(0,(49-contador))
+
+        print(listaPergunta[question])
+
+        resposta = input("Sua resposta é: ").strip().lower()
+
+        while resposta not in ("a","b","c","d","e","p"):
+            resposta = input("""Resposta invalida.   Por favor digite uma resposta válida: """).strip().lower()
+
+        if resposta != listaResposta[question]:
+
+            system("cls")
+            print("Que pena você errou e pedeu TUDO!")
+            errou = True
+
+            while denovo not in ("s","n"):
+                denovo = input(f"""Você deseja jogar novamente? [S/N]
+                r: """).strip().lower()
+
+            if denovo == "s":
+                system("cls")
+
+                listaJogadores.append(nome)
+
+                listaPontuacao.append(contador)
+
+                telainicial()
+
+            else:
+                print()
+
+            system("cls")
+
+        else:
+            contador+=1
+            print("Parabéns você acaba de ganhar UM MILHÃO DE REAIS!! OH OH")
+            while denovo not in ("s","n"):
+                denovo = input(f"""Você deseja jogar novamente? [S/N]
+                r: """).strip().lower()
+
+            if denovo == "s":
+                system("cls")
+
+                listaJogadores.append(nome)
+
+                listaPontuacao.append(contador)
+
+                telainicial()
+
+            else:
+                print()
+
+            system("cls")
+
+def telainicial():#função que começa o jogo colocando nome e escolhendo jogar sair ou mostrar classificação
+    global nome
+    system("cls")
+    print("""
+                -------------------------------------------------------
+                --------------Bem vindo ao Show do milhão--------------
+                -------------------------------------------------------""")
+    sleep(0.5)
+
+    nome = input("Digite seu nome: ")
+
+    while nome=="":#impedidno que o usuário coloque um nome em branco
+        nome = input("Por favor, Digite um nome válido: ")
+    system("cls")
+    oquefazer()
+
+def oquefazer():
+
+    listaJogador = deepcopy(listaJogadores)
+
+    listaPontos = deepcopy(listaPontuacao)
+
+    rodando = 0
+    opcao = "9"
+    while opcao not in ("1","2","3"):#manter as respostas entre as 3 possiveis
+
+        if rodando != 0:
+            print("Opção inválida, por favor digite uma opção válida!")
+
+        opcao = input(f"""Bem vindo {nome}, o que fazer?
+                    Digite [1] para jogar!
+                    Digite [2] para ver classificação!
+                    Digite [3] para sair!
+
+                    R: """).strip().upper()
+        rodando+=1
+
+    if opcao == "1":
+
+        jogo()
+
+    elif opcao == "2":
+        system("cls")
+
+        print('{:^30} {:^10} {:^10}'.format('Jogador', '-', 'Pontuação'))
+
+        for pessoa in range(len(listaJogador)):
+
+            print('-' * 50)
+
+            print('{:^30} {:^10} {}{:<10}'.format(listaJogador[listaPontos.index(max(listaPontos))], '-', 'R$', (listaPremio[max(listaPontos)]*1000)))
+
+            del listaJogador[listaPontos.index(max(listaPontos))]
+
+            listaPontos.remove(max(listaPontos))
+
+
+        print()
+
+        print()
+
+        input("Digite qualquer tecla para voltar")
+
+        telainicial()
+
+    else:
+        sair = input("Deseja mesmo sair? [S/N]").strip().lower()
+        if sair=="s":
+            print()
+        else:
+            oquefazer()
+telainicial()
---
+++ lista.py
@@ -0,0 +1,410 @@
+listaPremio = [0, 1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 100, 200, 300, 400, 500]
+
+listaJogadores = ['Carlos', 'André', 'Matias', 'Lucas', 'José', 'Ronaldo', 'O própio Silvio Santos']
+
+listaPontuacao = [3, 4, 9, 5, 6, 7, 15]
+
+listaPergunta = ["""Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?
+
+a) Tem entre 2 a 4 litros. São retirados 450 mililitros
+b) Tem entre 4 a 6 litros. São retirados 450 mililitros
+c) Tem 10 litros. São retirados 2 litros
+d) Tem 7 litros. São retirados 1,5 litros
+e) Tem 0,5 litros. São retirados 0,5 litros""",
+
+"""De quem é a famosa frase “Penso, logo existo”?
+
+a) Platão
+b) Galileu Galilei
+c) Descartes
+d) Sócrates
+e) Francis Bacon""",
+
+"""De onde é a invenção do chuveiro elétrico?
+
+a) França
+b) Inglaterra
+c) Brasil
+d) Austrália
+e) Itália""",
+
+"""Quais o menor e o maior país do mundo?
+
+a) Vaticano e Rússia
+b) Nauru e China
+c) Mônaco e Canadá
+d) Malta e Estados Unidos
+e) São Marino e Índia""",
+
+"""Qual o nome do presidente do Brasil que ficou conhecido como Jango?
+
+a) Jânio Quadros
+b) Jacinto Anjos
+c) Getúlio Vargas
+d) João Figueiredo
+e) João Goulart""",
+
+"""Qual o grupo em que todas as palavras foram escritas corretamente?
+
+a) Asterístico, beneficiente, meteorologia, entertido
+b) Asterisco, beneficente, meteorologia, entretido
+c) Asterisco, beneficente, metereologia, entretido
+d) Asterístico, beneficiente, metereologia, entretido
+e) Asterisco, beneficiente, metereologia, entretido""",
+
+"""Qual o livro mais vendido no mundo a seguir à Bíblia?
+
+a) O Senhor dos Anéis
+b) Dom Quixote
+c) O Pequeno Príncipe
+d) Ela, a Feiticeira
+e) Um Conto de Duas Cidades""",
+
+"""Quantas casas decimais tem o número pi?
+
+a) Duas
+b) Centenas
+c) Trilhares
+d) Vinte
+e) Milhares""",
+
+"""Atualmente, quantos elementos químicos a tabela periódica possui?
+
+a) 113
+b) 109
+c) 108
+d) 118
+e) 92""",
+
+"""Quais os países que têm a maior e a menor expectativa de vida do mundo?
+
+a) Japão e Serra Leoa
+b) Austrália e Afeganistão
+c) Itália e Chade
+d) Brasil e Congo
+e) Estados Unidos e Angola""",
+
+"""O que a palavra legend significa em português?
+
+a) Legenda
+b) Conto
+c) História
+d) Lenda
+e) Legendário""",
+
+"""Qual o número mínimo de jogadores numa partida de futebol?
+
+a) 8
+b) 10
+c) 9
+d) 5
+e) 7""",
+
+"""Quais os principais autores do Barroco no Brasil?
+
+a) Gregório de Matos, Bento Teixeira e Manuel Botelho de Oliveira
+b) Miguel de Cervantes, Gregório de Matos e Danthe Alighieri
+c) Padre Antônio Vieira, Padre Manuel de Melo e Gregório de Matos
+d) Castro Alves, Bento Teixeira e Manuel Botelho de Oliveira
+e) Álvares de Azevedo, Gregório de Matos e Bento Teixeira""",
+
+"""Quais as duas datas que são comemoradas em novembro?
+
+a) Independência do Brasil e Dia da Bandeira
+b) Proclamação da República e Dia Nacional da Consciência Negra
+c) Dia do Médico e Dia de São Lucas
+d) Dia de Finados e Dia Nacional do Livro
+e) Black Friday e Dia da Árvore""",
+
+"""Quem pintou "Guernica"?
+
+a) Paul Cézanne
+b) Pablo Picasso
+c) Diego Rivera
+d) Tarsila do Amaral
+e) Salvador Dalí""",
+
+"""Quanto tempo a luz do Sol demora para chegar à Terra?
+
+a) 12 minutos
+b) 1 dia
+c) 12 horas
+d) 8 minutos
+e) segundos""",
+
+"""Qual a tradução da frase “Fabiano cogió su saco antes de salir”?
+
+a) Fabiano coseu seu paletó antes de sair
+b) Fabiano fechou o saco antes de sair
+c) Fabiano pegou seu paletó antes de sair
+d) Fabiano cortou o saco antes de cair
+e) Fabiano rasgou seu paletó antes de cair""",
+
+"""Qual a nacionalidade de Che Guevara?
+
+a) Cubana
+b) Peruana
+c) Panamenha
+d) Boliviana
+e) Argentina""",
+
+"""Quais são os três predadores do reino animal reconhecidos pela habilidade de caçar em grupo, se camuflar para surpreender as presas e possuir sentidos apurados, respectivamente:
+
+a) Tubarão branco, crocodilo e sucuri
+b) Tigre, gavião e orca
+c) Hiena, urso branco e lobo cinzento
+d) Orca, onça e tarântula
+e) Leão, tubarão branco e urso cinzento""",
+
+"""Qual a altura da rede de vôlei nos jogos masculino e feminino?
+
+a) 2,4 para ambos
+b) 2,5 m e 2,0 m
+c) 1,8 m e 1,5 m
+d) 2,45 m e 2,15 m
+e) 2,43 m e 2,24 m""",
+
+"""Em que ordem surgiram os modelos atômicos?
+
+a) Thomson, Dalton, Rutherford, Rutherford-Bohr
+b) Rutherford-Bohr, Rutherford, Thomson, Dalton
+c) Dalton, Rutherford-Bohr, Thomson, Rutherford
+d) Dalton, Thomson, Rutherford-Bohr, Rutherford
+e) Dalton, Thomson, Rutherford, Rutherford-Bohr""",
+
+"""Qual personagem folclórico costuma ser agradado pelos caçadores com a oferta de fumo?
+
+a) Caipora
+b) Saci
+c) Lobisomem
+d) Boitatá
+e) Negrinho do Pastoreio""",
+
+"""Em que período da pré-história o fogo foi descoberto?
+
+a) Neolítico
+b) Paleolítico
+c) Idade dos Metais
+d) Período da Pedra Polida
+e) Idade Média""",
+
+"""Qual das alternativas abaixo apenas contêm classes de palavras?
+
+a) Vogais, semivogais e consoantes
+b) Artigo, verbo transitivo e verbo intransitivo
+c) Fonologia, Morfologia e Sintaxe
+d) Hiatos, ditongos e tritongos
+e) Substantivo, verbo e preposição""",
+
+"""Qual a montanha mais alta do Brasil?
+
+a) Pico da Neblina
+b) Pico Paraná
+c) Monte Roraima
+d) Pico Maior de Friburgo
+e) Pico da Bandeira""",
+
+"""Qual a velocidade da luz?
+
+a) 300 000 000 metros por segundo (m/s)
+b) 150 000 000 metros por segundo (m/s)
+c) 199 792 458 metros por segundo (m/s)
+d) 299 792 458 metros por segundo (m/s)
+e) 30 000 000 metros por segundo (m/s)""",
+
+"""Em qual local da Ásia o português é língua oficial?
+
+a) Índia
+b) Filipinas
+c) Moçambique
+d) Macau
+e) Portugal""",
+
+"""“It is six twenty ou twenty past six”. Que horas são em inglês?
+
+a) 12:06
+b) 6:20
+c) 2:20
+d) 6:02
+e) 12:20""",
+
+"""Quem é o autor de “O Príncipe”?
+
+a) Maquiavel
+b) Antoine de Saint-Exupéry
+c) Montesquieu
+d) Thomas Hobbes
+e) Rousseau
+""",
+
+"""Como é a conjugação do verbo caber na 1.ª pessoa do singular do presente do indicativo?
+
+a) Eu caibo
+b) Ele cabe
+c) Que eu caiba
+d) Eu cabo
+e) Nenhuma das alternativas""",
+
+"""Quais destas construções famosas ficam nos Estados Unidos?
+
+a) Estátua da Liberdade, Golden Gate Bridge e Empire State Building
+b) Estátua da Liberdade, Big Ben e The High Line
+c) Angkor Wat, Taj Mahal e Skywalk no Grand Canyon
+d) Lincoln Memorial, Sidney Opera House e Burj Khalifa
+e) 30 St Mary Axe, The High Line e Residencial 148 Spruce Street""",
+
+"""Quais destas doenças são sexualmente transmissíveis?
+
+a) Aids, tricomoníase e ebola
+b) Chikungunya, aids e herpes genital
+c) Gonorreia, clamídia e sífilis
+d) Botulismo, cistite e gonorreia
+e) Hepatite B, febre tifoide e hanseníase""",
+
+"""Qual destes países é transcontinental?
+
+a) Rússia
+b) Filipinas
+c) Istambul
+d) Groenlândia
+e) Tanzânia""",
+
+"""Em qual das orações abaixo a palavra foi empregada incorretamente?
+
+a) Mais uma vez, portou-se mal.
+b) É um homem mal.
+c) Esse é o mal de todos.
+d) Mal falou nele, o fulano apareceu.
+e) É um mau vendedor.""",
+
+"""Qual foi o recurso utilizado inicialmente pelo homem para explicar a origem das coisas?
+
+a) A Filosofia
+b) A Biologia
+c) A Matemática
+d) A Astronomia
+e) A Mitologia""",
+
+"""Qual das alternativas menciona apenas símbolos nacionais?
+
+a) Bandeira insígnia da presidência, bandeira nacional, brasão, hinos e selo
+b) Bandeira nacional, armas nacionais, hino nacional e selo nacional
+c) Bandeira nacional, brasão, hino nacional e hino da independência
+d) Bandeira nacional, cores nacionais, hino nacional e hino da independência
+e) Bandeira insígnia da presidência, brasão flora e fauna e hinos
+""",
+
+"""Quais os planetas do sistema solar?
+
+a) Terra, Vênus, Saturno, Urano, Júpiter, Marte, Netuno, Mercúrio
+b) Júpiter, Marte, Mercúrio, Netuno, Plutão, Saturno, Terra, Urano, Vênus
+c) Vênus, Saturno, Urano, Júpiter, Marte, Netuno, Mercúrio
+d) Júpiter, Marte, Mercúrio, Netuno, Plutão, Saturno, Sol, Terra, Urano, Vênus
+e) Terra, Vênus, Saturno, Júpiter, Marte, Netuno, Mercúrio""",
+
+"""Qual era o nome de Aleijadinho?
+
+a) Alexandrino Francisco Lisboa
+b) Manuel Francisco Lisboa
+c) Alex Francisco Lisboa
+d) Francisco Antônio Lisboa
+e) Antônio Francisco Lisboa""",
+
+"""Júpiter e Plutão são os correlatos romanos de quais deuses gregos?
+
+a) Ares e Hermes
+b) Cronos e Apolo
+c) Zeus e Hades
+d) Dionísio e Deméter
+e) Zeus e Ares""",
+
+"""Qual o maior animal terrestre?
+
+a) Baleia Azul
+b) Dinossauro
+c) Elefante africano
+d) Tubarão Branco
+e) Girafa""",
+
+"""Qual o tema do famoso discurso Eu Tenho um Sonho, de Martin Luther King?
+
+a) Igualdade das raças
+b) Justiça para os menos favorecidos
+c) Intolerância religiosa
+d) Prêmio Nobel da Paz
+e) Luta contra o Apartheid""",
+
+"""Que líder mundial ficou conhecida como “Dama de Ferro”?
+
+a) Dilma Rousseff
+b) Angela Merkel
+c) Margaret Thatcher
+d) Hillary Clinton
+e) Christine Lagarde""",
+
+"""O que são Acordo de Paris e Tríplice Aliança respectivamente?
+
+a) Acordo ortográfico entre países cuja língua oficial é o francês e Acordo de cooperação financeira internacional entre as três maiores potências mundiais
+b) Acordo entre países europeus acerca da imigração e Acordo econômico entre Inglaterra, Rússia a França
+c) Acordo entre vários países acerca das consequências do aquecimento global e Acordo de cooperação financeira internacional entre as três maiores potências mundiais
+d) Acordo de cooperação financeira internacional entre as três maiores potências mundiais e Acordo entre vários países acerca das consequências do aquecimento global
+e) Acordo entre vários países acerca das consequências do aquecimento global e Acordo entre Alemanha, império Austro-Húngaro e Itália acerca de apoio em caso de guerra""",
+
+"""Quais os nomes dos três Reis Magos?
+
+a) Gaspar, Nicolau e Natanael
+b) Belchior, Gaspar e Baltazar
+c) Belchior, Gaspar e Nataniel
+d) Gabriel, Benjamim e Melchior
+e) Melchior, Noé e Galileu""",
+
+"""Quais os principais heterônimos de Fernando Pessoa?
+
+a) Alberto Caeiro, Ricardo Reis e Álvaro de Campos
+b) Ariano Suassuna, Raul Bopp e Quincas Borba
+c) Bento Teixeira, Ricardo Reis e Haroldo de Campos
+d) Alberto Caeiro, Ricardo Leite e Augusto de Campos
+e) Bento Teixeira, Ricardo Reis e Augusto de Campos""",
+
+"""Qual a religião monoteísta que conta com o maior número de adeptos no mundo?
+
+a) Judaísmo
+b) Zoroastrismo
+c) Islamismo
+d) Cristianismo
+e) Hinduísmo""",
+
+"""Qual desses filmes foi baseado na obra de Shakespeare?
+
+a) Muito Barulho por Nada (2012)
+b) Capitães de Areia (2011)
+c) A Dama das Camélias (1936)
+d) A Revolução dos Bichos (1954)
+e) Excalibur (1981)""",
+
+"""Quem foi o primeiro homem a pisar na Lua? Em que ano aconteceu?
+
+a) Yuri Gagarin, em 1961
+b) Buzz Aldrin, em 1969
+c) Charles Conrad, em 1969
+d) Charles Duke, em 1971
+e) Neils Armstrong, em 1969.""",
+
+"""Qual o nome do cientista que descobriu o processo de pasteurização e a vacina contra a raiva?
+
+a) Marie Curie
+b) Blaise Pascal
+c) Louis Pasteurs
+d) Antoine Lavoisier
+e) Charles Darwin""",
+
+"""As pessoas de qual tipo sanguíneo são consideradas doadores universais?
+
+a) Tipo A
+b) Tipo B
+c) Tipo O
+d) Tipo AB
+e) Tipo ABO""",
+]
+
+listaResposta = ["b","c","c","a","e","b","b","c","d","a","d","e","a","b","b","d","c","e","c","e","e","a","b","e","a","d","d","b","a","a","a","c","a","b","e","b","a","e","c","c","a","c","e","b","a","d","a","e","c","c",]
